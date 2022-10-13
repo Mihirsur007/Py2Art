4813eee1aaa7448bac5d77c201c1c9052d2863eb
Mihirsur007 <74442956+Mihirsur007@users.noreply.github.com>
2022-10-12 22:38:29 -0700

    Add files via upload

---
+++ Py2App.py
@@ -0,0 +1,145 @@
+# Please read the license before using this software
+
+from turtle import *
+from random import randint
+import turtle
+from tkinter import *
+from tkmacosx import Button
+
+x = ["red", 'orange', 'yellow', 'spring green', 'cyan', 'dark orchid', 'hot pink', 'cornflower blue']
+
+def setup():
+    turtle.showturtle()
+    ws = turtle.Screen()
+    turtle.speed(0)
+    ws.bgcolor("black")
+
+def makeSquare():
+    turtle.begin_fill()
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.end_fill()
+
+
+def reset():
+    turtle.forward(-240)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+
+def frame():
+    turtle.right(90)
+    turtle.color('black')
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.color("white")
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+
+def makeart():
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+    reset()
+
+    for c in range(0,len(x)):
+        turtle.color(x[randint(0, (len(x) - 1))])
+        makeSquare()
+
+################################
+#Create an instance of window
+win = Tk()
+win.configure(bg="black")
+win.title("Py2Art")
+
+#Set the geometry of the window
+win.geometry("700x400")
+
+#Define a function to close the window
+def close_win():
+   win.destroy()
+
+def clicked():
+    setup()
+    win.destroy()
+    makeart()
+    frame()
+    turtle.hideturtle()
+    turtle.done()
+
+
+#Create a label
+Label(win, text="Py2Art",
+font=('Poppins bold', 35), bg="black", fg='Cyan').pack(pady= 20)
+Label(win, text="A Python Art Generator",
+font=('Poppins bold', 25), bg="black", fg='Cyan').pack(pady= 20)
+#Create a Button
+Button(win, text= "Start", font=('Poppins bold', 16),
+command=clicked, bg="black", fg='Cyan').pack(pady=20)
+
+
+Button(win, text= "Quit", font=('Poppins bold', 16),
+command=close_win, bg="black", fg='Cyan').pack(pady=20)
+
+Label(win, text="Created by Mihir Surlaker",
+font=('Poppins bold', 15), bg="black", fg='Cyan').pack(pady= 20)
+
+win.mainloop()
+
+
+
+
+
+
